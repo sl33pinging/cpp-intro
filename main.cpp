6eaf00f19bb8da12d273527bdeb3b4808a604d19
Daniel Song <danielsong1226@gmail.com>
2023-07-18 14:16:04 -0400

    Setup CMake and coded starting assignments.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
 
-int main()
-{
-  std::cout << "Hello, world!" << std::endl;
+int main(){
+  std::string name = "Daniel";
+  std::string classroom = "115";
+  std::cout << "My name is" << name << std::endl;
+  std::cout << "My classroom is " << classroom << std::endl;
 
   int x = 5;
   int y = 7;
-  std::cout << "The sum of " << x << " and " << y << " is " << x + y << std::endl;
+  int z = x + y;
+  int w = x * y * z;
+  float v = x / (float) y;
+
+  std::cout << "The sum of " << x << " and " << y << " is " << z << std::endl;
+  std::cout << "The product of " << x << ", " << y << ",and " << z << " is " << w << std::endl;
+  std::cout << "The quotient of " << x << " and " << y << " is " << v << std::endl;
+
 
   return 0;
+}
+
+int subtract(int x, int y){
+  return x - y;
+}
+
+int multiply(int x, int y){
+  return x * y;
+}
+
+int divide(int x, int y){
+  return x / y;
 }
